eaf071a1335d11df4f9439415a39e4120bcc1057
unknown <osborne.saka@meltwater.org>
2018-11-26 22:04:45 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+ * 
+ *Primary file for api 
+ *  
+ */ 
+
+
+//Dependencies
+const http = require('http')
+
+//The server should respond to all requests with a string
+var server = http.createServer((req,res)=>
+    res.end('Hello World \n')
+)
+
+//Start the server, and have it listen on port 3000
+server.listen(3000,()=> console.log('The server is listening on port 3000 now'))
